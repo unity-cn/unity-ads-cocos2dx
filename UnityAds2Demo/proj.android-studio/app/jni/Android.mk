2fe64fa84d5b7ca3c4eb91b293acb6bd052a6961
Callie Zhu <zhujiali@unity3d.com>
2019-02-21 14:18:17 +0800

    Update CMakeLists and Android.mk to complie UnityMonetizationJNI.cpp file.

--- UnityAds2Demo/proj.android-studio/app/jni/Android.mk
+++ UnityAds2Demo/proj.android-studio/app/jni/Android.mk
@@ -2,34 +2,30 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/external)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos)
+LOCAL_MODULE := MyGame_shared
 
-LOCAL_MODULE := cocos2dcpp_shared
+LOCAL_MODULE_FILENAME := libMyGame
 
-LOCAL_MODULE_FILENAME := libcocos2dcpp
-
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   hellocpp/UnityAdsJNI.cpp \
-                   ../../../Classes/AppDelegate.cpp \
-                   ../../../Classes/HelloWorldScene.cpp
+LOCAL_SRC_FILES := $(LOCAL_PATH)/hellocpp/main.cpp \
+                   $(LOCAL_PATH)/../../../Classes/AppDelegate.cpp \
+                   $(LOCAL_PATH)/../../../cocos2dx/cocos/platform/android/jni/UnityMonetizationJNI.cpp \
+                   $(LOCAL_PATH)/../../../Classes/HelloWorldScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
-                    hellocpp/UnityAdsNativeAPI.h
+$(LOCAL_PATH)/../../../cocos2dx/cocos/platform/android/jni
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
 
 
-LOCAL_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_STATIC_LIBRARIES := cc_static
 
 # _COCOS_LIB_ANDROID_BEGIN
 # _COCOS_LIB_ANDROID_END
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,.)
+$(call import-module, cocos)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
